2f0c9c538b21545e9d41d8de23b36847f620ab60
Dipanshu Sharma <63792712+Dipanshuonline@users.noreply.github.com>
2023-10-06 11:21:11 +0530

    Update Selenium.java

--- src/test/java/CicdSelenium/Selenium.java
+++ src/test/java/CicdSelenium/Selenium.java
@@ -2,6 +2,7 @@ package CicdSelenium;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
